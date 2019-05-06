f374ba64524b11d8416b1e345a7c959733bffb48
ManiShankarGajavelli <gmanishanker@gmail.com>
2019-05-06 08:51:48 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+var CACHE_NAME = 'NB-Static-Cache-v1';
+var urlsToCache = [
+    '/',
+  '/index.html',
+  'NewsScript.js',
+  'bootstrap.min.js',
+  'bootstrap.min.css',
+  'style.css',
+  'jquery3.2.1.min.js',
+  'fontawesome.js',
+  'fontawesome.css'
+];
+
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+          .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+          }).then(function(out){
+            console.log(out);
+        }).catch(function(e){
+        console.log(e);
+    })
+      );
+},function(err) {
+    // registration failed 
+    console.log('ServiceWorker install failed: ', err);
+  });
+
+self.addEventListener('fetch', function(event) {
+    console.log("Fetch event is called");
+    event.respondWith(
+        caches.match(event.request)
+          .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+              return response;
+            }
+            return fetch(event.request);
+          }
+        )
+      );
+},function(err) {
+    // registration failed 
+    console.log('ServiceWorker fetch failed: ', err);
+  });
+
+self.addEventListener('activate', function(event) {
+console.log('activate event is called');
+});
